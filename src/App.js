d443fdeccd22ccee5b389f40739cb7b8be79c553
Jose <jocvegar@gmail.com>
2020-08-14 20:03:24 -0600

    fixes typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This will be my mine page!
+          This will be my main page!
         </p>
       </header>
     </div>
