d170c81b92c517da5e55d1c06b8684ae747322c9
Jose <jocvegar@gmail.com>
2020-08-16 10:02:04 -0600

    Adds delete method

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component }from 'react';
 import axios from 'axios';
 
-class Home extends React.Component {
-  state = {
-    candidates: []
+class Home extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      candidates: []
+    };
+
+    this.handleRemove = this.handleRemove.bind(this);
   }
 
   setToken() {
@@ -14,7 +19,6 @@ class Home extends React.Component {
 
   componentDidMount() {
     const apiURL = process.env.REACT_APP_API_URL
-    // axios.get(`https://acklen-avenue-backend.herokuapp.com/api/candidates`)
     let token = this.setToken()
     axios.get(`${apiURL}/candidates`, {
       headers: { Authorization: `Bearer ${token}` }
@@ -24,15 +28,63 @@ class Home extends React.Component {
       this.setState({ candidates });
     })
   }
+
+  removeCandidate(index) {
+    const candidates = [...this.state.candidates]
+    candidates.splice(index, 1);
+
+    this.setState({
+      candidates
+    });
+  }
+
+  handleRemove(slug, index) {
+    const apiURL = process.env.REACT_APP_API_URL
+    let token = this.setToken()
+    axios.delete(`${apiURL}/candidates/${slug}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(() => {
+      this.removeCandidate(index)
+    }).catch((e) => console.log(e))
+  }
+
+
   render() {
    return(
-    <div> 
-      <h1>Hola!</h1> 
-      <ul>
-        { this.state.candidates.map(candidate => 
-          <li key={candidate.slug}>{candidate.email}</li>
+    <div className="container"> 
+      <h1 className="text-center pt-3 pb-5">Candidates</h1> 
+      <div className="responsive">
+      <table className="table table-hover table-bordered">
+        <thead>
+        <tr>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Status</th>
+          <th scope="col">Delete?</th>
+        </tr>
+        </thead>
+        <tbody>
+        { this.state.candidates.map((candidate, idx) => 
+          <tr key={candidate.slug}>
+            <td>{candidate.first_name}</td>
+            <td>{candidate.last_name}</td>
+            <td>{candidate.email}</td>
+            <td>{candidate.status}</td>
+            <td className="text-center">
+              <button 
+                type="button" 
+                className="btn btn-danger btn-sm"
+                onClick={() => this.handleRemove(candidate.slug, idx)}>
+                Remove
+              </button>
+          </td>
+          </tr>
         )}
-      </ul>
+        </tbody>
+      </table>
+      </div>
     </div>
    )
   }
