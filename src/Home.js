f5db634edcdfc9e40db0936937c6ca872fb6ab05
Jose <jocvegar@gmail.com>
2020-08-16 11:57:34 -0600

    Adds create

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
 import React, { Component }from 'react';
 import axios from 'axios';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class Home extends Component {
   constructor(props) {
     super();
     this.state = {
-      candidates: []
+      candidates: [],
+      showModal: false,
+      first_name: "",
+      last_name: "",
+      email: "",
     };
 
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleNewFormChange = this.handleNewFormChange.bind(this);
+    this.submitNewForm = this.submitNewForm.bind(this);
   }
 
   setToken() {
@@ -18,6 +27,10 @@ class Home extends Component {
   }
 
   componentDidMount() {
+    this.getCandidates()
+  }
+
+  getCandidates() {
     const apiURL = process.env.REACT_APP_API_URL
     let token = this.setToken()
     axios.get(`${apiURL}/candidates`, {
@@ -49,11 +62,62 @@ class Home extends Component {
     }).catch((e) => console.log(e))
   }
 
+  handleModalHide() {
+    this.setState({ showModal: false });
+  }
 
+  handleModalShow() {
+    this.setState({ showModal: true });
+  }
+
+  handleNewFormChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  submitNewForm() {
+    const apiURL = process.env.REACT_APP_API_URL
+    let token = this.setToken()
+    axios.post(`${apiURL}/candidates`, {
+      candidate: {
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        email: this.state.email,
+      },
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(()=> {
+      this.setState({ 
+        first_name: "",
+        last_name: "",
+        email: ""
+      });
+      this.handleModalHide()
+      this.getCandidates()
+    })
+    .catch((e) => console.log(e))
+  }
+  
   render() {
    return(
     <div className="container"> 
       <h1 className="text-center pt-3 pb-5">Candidates</h1> 
+      <div className="row pb-4 text-center">
+        <div className="col-6">
+          <button 
+            type="button" 
+            className="btn btn-outline-success btn-sm btn-block"
+            onClick={() => this.handleModalShow()}>
+            + Add
+          </button>
+        </div>
+        <div className="col-6">
+        <button 
+          type="button" 
+          className="btn btn-outline-secondary btn-sm btn-block">
+          Sort
+        </button>
+        </div>
+      </div>
       <div className="responsive">
       <table className="table table-hover table-bordered">
         <thead>
@@ -76,7 +140,7 @@ class Home extends Component {
               <button 
                 type="button" 
                 className="btn btn-danger btn-sm"
-                onClick={() => this.handleRemove(candidate.slug, idx)}>
+                onClick={() => {if(window.confirm('Are you sure to delete this candidate?'))this.handleRemove(candidate.slug, idx)}}>
                 Remove
               </button>
           </td>
@@ -85,6 +149,53 @@ class Home extends Component {
         </tbody>
       </table>
       </div>
+
+      <Modal show={this.state.showModal} false onHide={() => this.handleModalHide()}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add new candidate</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Control 
+                type="text"
+                name="first_name" 
+                placeholder="first name" 
+                size="sm"
+                value={this.state.first_name}
+                onChange={this.handleNewFormChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput2">
+              <Form.Control 
+                type="text" 
+                name="last_name" 
+                placeholder="last name" 
+                size="sm" 
+                onChange={this.handleNewFormChange}/>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput3">
+              <Form.Control 
+                type="email" 
+                name="email" 
+                placeholder="email@example.com" 
+                size="sm" 
+                onChange={this.handleNewFormChange}/>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => this.handleModalHide()}>
+            Cancel
+          </Button>
+          <Button variant="primary" 
+            onClick={this.submitNewForm}
+            disabled = {!this.state.first_name || !this.state.last_name || !this.state.email}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+  
     </div>
    )
   }
